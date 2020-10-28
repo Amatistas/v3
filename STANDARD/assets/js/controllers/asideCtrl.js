8b0880def8ebfdb3e0d50e8f0a5b18ced2f1c393
adolfogbztt <adolfo.gbztt@gmail.com>
2020-10-27 20:16:16 -0500

    bug fix

--- STANDARD/assets/js/controllers/asideCtrl.js
+++ STANDARD/assets/js/controllers/asideCtrl.js
@@ -1300,7 +1300,7 @@ app.controller('AsideModalTransaccionCtrl', [
 						where: 'cod',
 						key: '',
 						mostrar: [ 'cod', 'descripcion' ],
-						panel: 'nubefapanel'
+						panel: 'nubefaco_nubefa'
 					};
 
 					$scope.rr.td_id = {
@@ -1362,7 +1362,7 @@ app.controller('AsideModalTransaccionCtrl', [
 						var arr = obj.selectId;
 
 						if (param.panel) {
-							var bd = 'nubefapanel';
+							var bd = 'nubefaco_nubefa';
 						} else {
 							var bd = JSON.parse($rootScope.d.datos).database;
 						}
@@ -2225,7 +2225,7 @@ app.controller('AsideModalTransaccionCtrl', [
 						where: 'cod',
 						key: '',
 						mostrar: [ 'cod', 'descripcion' ],
-						panel: 'nubefapanel'
+						panel: 'nubefaco_nubefa'
 					};
 					$rootScope.buscarOption = function(param) {
 						switch (typeof param) {
@@ -2242,7 +2242,7 @@ app.controller('AsideModalTransaccionCtrl', [
 						var arr = obj.selectId;
 
 						if (param.panel) {
-							var bd = 'nubefapanel';
+							var bd = 'nubefaco_nubefa';
 						} else {
 							var bd = JSON.parse($rootScope.d.datos).database;
 						}
@@ -3678,7 +3678,7 @@ app.controller('AsideModalTransaccionCtrl', [
 						where: 'cod',
 						key: '',
 						mostrar: [ 'cod', 'descripcion' ],
-						panel: 'nubefapanel'
+						panel: 'nubefaco_nubefa'
 					};
 
 					$scope.guardarNotaCredito = function() {
