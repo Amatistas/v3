0018c3c966b7409c6bbad14909bd44ed76c1e241
adolfogbztt <adolfo.gbztt@gmail.com>
2020-11-09 17:35:29 -0500

    bug fix pdf
    Co-authored-by: Andrys Salazar <design@social-umbrella.com>

--- compass/view/FPDF.php
+++ compass/view/FPDF.php
@@ -68,176 +68,200 @@ class PDF extends FPDF
 
     function Header()
     {
-        
-    function tipo($type){
-        switch ($type) {
-            case 'FA':
-                $var = "FACTURA ELECTRÓNICA";
-                break;
-            case 'BO':
-                $var = "BOLETA ELECTRÓNICA";
-                break;
-            
-            case 'CT':
-                $var = "COTIZACIÓN";
-                break;
-            case 'GR':
-                $var = "GUIA DE REMISIÓN ELECTRÓNICA";
-                break;
-            case 'Pagos':
-                $var = "COMPROBANTE DE EGRESO";
-                break;
+
+        function tipo($type)
+        {
+            switch ($type) {
+                case 'FA':
+                    $var = "FACTURA ELECTRÓNICA";
+                    break;
+                case 'BO':
+                    $var = "BOLETA ELECTRÓNICA";
+                    break;
+
+                case 'CT':
+                    $var = "COTIZACIÓN";
+                    break;
+                case 'GR':
+                    $var = "GUIA DE REMISIÓN ELECTRÓNICA";
+                    break;
+                case 'Pagos':
+                    $var = "COMPROBANTE DE EGRESO";
+                    break;
                 case 'NC':
                     $var = "NOTA DE CRÉDITO";
-                break;  
-            case 41:
-                $var = "COMPROBANTE DE CARGA";
-                break;
-            case 40:
-                $var = "COMPROBANTE DE SALIDA INTERNA";
-                break;
-            case 42:
-                $var = "COMPROBANTE DE DESCARGA";
-                break;
-            case 43:
-                $var = "COMPROBANTE DE ENTRADA INTERNA";
-                break;
-            case 11:
-                $var = "DNI";
-                break;
-            case 23:
-                $var = "RUC";
-                break;
-            default:
-                $var = "NA";
-                break;
+                    break;
+                case 41:
+                    $var = "COMPROBANTE DE CARGA";
+                    break;
+                case 40:
+                    $var = "COMPROBANTE DE SALIDA INTERNA";
+                    break;
+                case 42:
+                    $var = "COMPROBANTE DE DESCARGA";
+                    break;
+                case 43:
+                    $var = "COMPROBANTE DE ENTRADA INTERNA";
+                    break;
+                case 11:
+                    $var = "DNI";
+                    break;
+                case 23:
+                    $var = "RUC";
+                    break;
+                default:
+                    $var = "NA";
+                    break;
+            }
+            return $var;
         }
-        return $var;
-      }
-    function tipo2($type){
-        switch ($type) {
-    
-            case 13:
-                $var = array('peso', 'total');
-                break;
-            default:
-                $var = array('precio', 'importe');
-                break;
-        }
-        return $var;
-      }
-     
-//   var_dump($this->arry);
-//   var_dump($this->arry2);
-//   var_dump($this->arry3);
-//   var_dump($this->arry4);
-//   var_dump($this->arry5);
-//   var_dump($this->arry6);
-                   // Logo
-                 $this->Image("../../api/upload/".substr($this->arry3[0]['fe_log'],2), 10, 8, 33);
-                 
-                   $this->SetFont('Arial', 'I', 8);
-           
-                   $this->SetXY(55, 8);
-                   $this->MultiCell(100, 4, $this->arry3[0]['emp_nom'] . "\n " . substr($this->arry3[0]['emp_dir'], 0, 47) . "\n" . substr($this->arry3[0]['departamento'] . " - " . $this->arry3[0]['provincia'] . " - " . $this->arry3[0]['distrito'], 0, 59) . "\n" . substr("Email: " . $this->arry3[0]['emp_ema'], 0, 59) . "\n" . substr("Teléf: " . $this->arry3[0]['emp_tel'] . " Cel: " . $this->arry3[0]['emp_cel'], 0, 59) . " \n", 0, 'L');
-              
 
-  
-        if($this->arry[0]['to_id'] == 13){
+        function tipo2($type)
+        {
+            switch ($type) {
+                case 13:
+                    $var = array('peso', 'total');
+                    break;
+                default:
+                    $var = array('precio', 'importe');
+                    break;
+            }
+            return $var;
+        }
+
+        //   var_dump($this->arry);
+        //   var_dump($this->arry2);
+        //   var_dump($this->arry3);
+        //   var_dump($this->arry4);
+        //   var_dump($this->arry5);
+        //   var_dump($this->arry6);
+        // Logo
+        $this->Image("../../api/upload/" . substr($this->arry3[0]['fe_log'], 2), 10, 8, 33);
+
+        $this->SetFont('Arial', 'I', 8);
+
+        $this->SetXY(55, 8);
+        $this->MultiCell(100, 4, $this->arry3[0]['emp_nom'] . "\n " . substr($this->arry3[0]['emp_dir'], 0, 47) . "\n" . substr($this->arry3[0]['departamento'] . " - " . $this->arry3[0]['provincia'] . " - " . $this->arry3[0]['distrito'], 0, 59) . "\n" . substr("Email: " . $this->arry3[0]['emp_ema'], 0, 59) . "\n" . substr("Teléf: " . $this->arry3[0]['emp_tel'] . " Cel: " . $this->arry3[0]['emp_cel'], 0, 59) . " \n", 0, 'L');
+
+
+
+        if ($this->arry[0]['to_id'] == 13) {
             $this->SetFont('Arial', 'B', 9);
             $this->SetXY(150, 10);
-            $this->MultiCell(50, 5, "RUC" . $this->arry3[0]['emp_ruc'] . "\n " . tipo($this->arry[0]['td_id'])  ."\n". $this->arry[0]['documento'], 1, 'C');
+            $this->MultiCell(50, 5, "RUC" . $this->arry3[0]['emp_ruc'] . "\n " . tipo($this->arry[0]['td_id'])  . "\n" . $this->arry[0]['documento'], 1, 'C');
             // Line break
             $this->SetMargins(10, 30, 20, 20);
-    
+
             $this->SetFont('Arial', '', 9);
             $this->SetXY(10, 30);
-            $this->MultiCell(110, 5, "Cliente: " . substr($this->arry[0]['ane_razsoc'], 0, 49) . "\n" . tipo($this->arry[0]['ane_tipdoc']).": " . $this->arry[0]['ane_numdoc'] . "\n" . "Dirección: " . substr($this->arry[0]['ane_dir'] . "\n" . "Ubigeo: " . $this->arry[0]['departamento'] . "\n" . "OC: ".$this->arry[0]['ven_obs'], 0, 70), 0, 'L');
-           $this->SetXY(120, 30);
-            $this->MultiCell(80, 5, "Fecha Emisión: " . $this->arry[0]['ven_fecreg'] . "\n" ."Pto Partida: ".substr($this->arry[0]['ane_dir']." - ". $this->arry[0]['provincia']." - " . $this->arry[0]['departamento'], 0, 70) . "\n"."Pto Llegada: ".$this->arry[0]['ven_direccion_destino']." - ".$this->arry[0]['provincia']." - ".$this->arry[0]['departamento'] ."\n", 0, 'L');
+            $this->MultiCell(110, 5, "Cliente: " . substr($this->arry[0]['ane_razsoc'], 0, 49) . "\n" . tipo($this->arry[0]['ane_tipdoc']) . ": " . $this->arry[0]['ane_numdoc'] . "\n" . "Dirección: " . substr($this->arry[0]['ane_dir'] . "\n" . "Ubigeo: " . $this->arry[0]['departamento'] . "\n" . "OC: " . $this->arry[0]['ven_obs'], 0, 70), 0, 'L');
+            $this->SetXY(120, 30);
+            $this->MultiCell(80, 5, "Fecha Emisión: " . $this->arry[0]['ven_fecreg'] . "\n" . "Pto Partida: " . substr($this->arry[0]['ane_dir'] . " - " . $this->arry[0]['provincia'] . " - " . $this->arry[0]['departamento'], 0, 70) . "\n" . "Pto Llegada: " . $this->arry[0]['ven_direccion_destino'] . " - " . $this->arry[0]['provincia'] . " - " . $this->arry[0]['departamento'] . "\n", 0, 'L');
             $this->SetLineWidth(0.4);
             $this->line(10, 55, 200, 55);
             $this->SetY(60);
             // $fpdf->Ln(10);
             $this->SetFont('Arial', 'B', 8);
-    
+
             $this->cell(20, 6, 'Código', 1, '', 'C');
             $this->cell(90, 6, 'Descripción', 1, '', 'C');
             $this->cell(20, 6, 'Cant', 1, '', 'C');
             // $this->cell(15,6,'Psnt',1,'','C');
-            $col=tipo2($this->arry[0]['to_id']);
+            $col = tipo2($this->arry[0]['to_id']);
             $this->cell(30, 6, $col[0], 1, '', 'C');
             $this->cell(30, 6, $col[1], 1, '', 'C');
             $this->Ln(8);
-        }else if($this->arry[0]['to_id'] == 41||$this->arry[0]['to_id'] == 40||$this->arry[0]['to_id'] == 42||$this->arry[0]['to_id'] == 43){
+        } else if ($this->arry[0]['to_id'] == 41 || $this->arry[0]['to_id'] == 40 || $this->arry[0]['to_id'] == 42 || $this->arry[0]['to_id'] == 43) {
             $this->SetFont('Arial', 'B', 9);
             $this->SetXY(140, 10);
-            $this->MultiCell(60, 5, "RUC" . $this->arry3[0]['emp_ruc'] . "\n " . tipo($this->arry[0]['to_id'])  ."\n". $this->arry[0]['documento'], 1, 'C');
+            $this->MultiCell(60, 5, "RUC" . $this->arry3[0]['emp_ruc'] . "\n " . tipo($this->arry[0]['to_id'])  . "\n" . $this->arry[0]['documento'], 1, 'C');
             // Line break
             $this->SetMargins(10, 30, 20, 20);
-    
+
             $this->SetFont('Arial', '', 9);
             $this->SetXY(10, 33);
-            $this->MultiCell(110, 5,"Dirección: " . substr($this->arry[0]['alm_dir'] . "\n" . "Ubigeo: " . $this->arry[0]['departamento'] . " - " . $this->arry[0]['provincia'], 0, 70), 0, 'L');
-           $this->SetXY(120, 33);
-            $this->MultiCell(80, 5, "Fecha de Ingreso: " . $this->arry[0]['alm_fecha'] . "\n" ."Fecha de Traslado: ".$this->arry[0]['fec_tras'], 0, 'L');
+            $this->MultiCell(110, 5, "Dirección: " . substr($this->arry[0]['alm_dir'] . "\n" . "Ubigeo: " . $this->arry[0]['departamento'] . " - " . $this->arry[0]['provincia'], 0, 70), 0, 'L');
+            $this->SetXY(120, 33);
+            $this->MultiCell(80, 5, "Fecha de Ingreso: " . $this->arry[0]['alm_fecha'] . "\n" . "Fecha de Traslado: " . $this->arry[0]['fec_tras'], 0, 'L');
             $this->SetLineWidth(0.4);
             $this->line(10, 45, 200, 45);
             $this->SetY(50);
-        }
-        else if($this->arry4!=NULL){    
+        } else if ($this->arry4 != NULL) {
             $this->SetFont('Arial', 'B', 9);
-            $this->SetXY(150, 10);   
-            $this->MultiCell(50, 5, "RUC" . $this->arry3[0]['emp_ruc'] . "\n " . "COMPROBANTE DE EGRESO"  ."\n", 1, 'C');
+            $this->SetXY(150, 10);
+            $this->MultiCell(50, 5, "RUC" . $this->arry3[0]['emp_ruc'] . "\n " . "COMPROBANTE DE EGRESO"  . "\n", 1, 'C');
             // Line break
             $this->SetMargins(10, 30, 20, 20);
-    
+
             $this->SetFont('Arial', '', 9);
             $this->SetXY(10, 30);
-            $this->MultiCell(110, 5, "Cta: " . substr($this->arry4[0]['cc_id']." ".$this->arry4[0]['ban_des']." - ".$this->arry4[0]['ctaban_num'], 0, 55) ."\n". "Fecha: ". $this->arry4[0]['fecha_factura'], 0, 'L');
+            $this->MultiCell(110, 5, "Cta: " . substr($this->arry4[0]['cc_id'] . " " . $this->arry4[0]['ban_des'] . " - " . $this->arry4[0]['ctaban_num'], 0, 55) . "\n" . "Fecha: " . $this->arry4[0]['fecha_factura'], 0, 'L');
             $this->SetXY(120, 30);
-            
-            $this->MultiCell(80, 5, "Tipo de Pago: ".$this->arry4[0]['tp_nom']."\n". "N° Operación: ".$this->arry4[0]['pag_numope'] , 0, 'L');
+
+            $this->MultiCell(80, 5, "Tipo de Pago: " . $this->arry4[0]['tp_nom'] . "\n" . "N° Operación: " . $this->arry4[0]['pag_numope'], 0, 'L');
             $this->SetLineWidth(0.4);
             $this->line(10, 40, 200, 40);
             $this->SetY(45);
-        } 
-        else{
+        } else {
+            $this->SetLineWidth(0.2);
+            $this->line(10, 67, 200, 67);
+            $this->SetLineWidth(0.2);
+            $this->line(10, 199, 200, 199);
+            $this->SetLineWidth(0.2);
+            $this->line(10, 67, 10, 199);
+            $this->SetLineWidth(0.2);
+            $this->line(30, 67, 30, 199);
+            $this->SetLineWidth(0.2);
+            $this->line(120, 67, 120, 199);
+            $this->SetLineWidth(0.2);
+            $this->line(135, 67, 135, 199);
+            $this->SetLineWidth(0.2);
+            $this->line(120, 67, 120, 199);
+            $this->SetLineWidth(0.2);
+            $this->line(150, 67, 150, 199);
+            $this->SetLineWidth(0.2);
+            $this->line(200, 67, 200, 199);
+            $this->SetLineWidth(0.2);
+            $this->line(175, 67, 175, 199);
             $this->SetFont('Arial', 'B', 9);
             $this->SetXY(150, 10);
-            $this->MultiCell(50, 5, "RUC" . $this->arry3[0]['emp_ruc'] . "\n " . tipo($this->arry[0]['td_id'])  ."\n". $this->arry[0]['documento'], 1, 'C');
+            $this->MultiCell(50, 5, "RUC" . $this->arry3[0]['emp_ruc'] . "\n " . tipo($this->arry[0]['td_id'])  . "\n" . $this->arry[0]['documento'], 1, 'C');
             // Line break
             $this->SetMargins(10, 30, 20, 20);
-    
+
             $this->SetFont('Arial', '', 9);
             $this->SetXY(10, 30);
-            $this->MultiCell(110, 5, "Cliente: " . substr($this->arry[0]['ane_razsoc'], 0, 49) . "\n" . tipo($this->arry[0]['ane_tipdoc']).": " . $this->arry[0]['ane_numdoc'] . "\n" . "Dirección: " . substr($this->arry[0]['ane_dir'] . "\n" . "Ubigeo: " . $this->arry[0]['departamento'] . "\n" . "OC: ".$this->arry[0]['ven_obs'], 0, 70), 0, 'L');
+            $this->MultiCell(110, 5, "Cliente: " . substr($this->arry[0]['ane_razsoc'], 0, 49) . "\n" . tipo($this->arry[0]['ane_tipdoc']) . ": " . $this->arry[0]['ane_numdoc'] . "\n" . "Dirección: " . substr($this->arry[0]['ane_dir'] . "\n" . "Ubigeo: " . $this->arry[0]['departamento'] . "\n" . "OC: " . $this->arry[0]['ven_obs'], 0, 70), 0, 'L');
             $this->SetXY(120, 30);
-            $this->MultiCell(80, 5, "Fecha Emisión: " . $this->arry[0]['ven_fecreg'] . "\n" . "Condición: ".$this->arry[0]['fp_nom']."\n" . "G.R Remitente: \n" . "G.R Transportista: ", 0, 'L');
-            $this->SetLineWidth(0.4);
+            $this->MultiCell(80, 5, "Fecha Emisión: " . $this->arry[0]['ven_fecreg'] . "\n" . "Condición: " . $this->arry[0]['fp_nom'] . "\n" . "G.R Remitente: \n" . "G.R Transportista: ", 0, 'L');
+            $this->SetLineWidth(0.2);
             $this->line(10, 55, 200, 55);
+            $this->line(10, 29, 200, 29);
+            $this->line(10, 29, 10, 55);
+            $this->line(200, 29, 200, 55);
             $this->SetY(60);
             // $fpdf->Ln(10);
             $this->SetFont('Arial', 'B', 8);
-    
+
             $this->cell(20, 6, 'Código', 1, '', 'C');
             $this->cell(90, 6, 'Descripción', 1, '', 'C');
-            $this->cell(20, 6, 'Cant', 1, '', 'C');
+            $this->cell(15, 6, 'Cant', 1, '', 'C');
+            $this->cell(15, 6, 'Psnt', 1, '', 'C');
             // $this->cell(15,6,'Psnt',1,'','C');
-            $col=tipo2($this->arry[0]['to_id']);
-            $this->cell(30, 6, $col[0], 1, '', 'C');
-            $this->cell(30, 6, $col[1], 1, '', 'C');
+            $col = tipo2($this->arry[0]['to_id']);
+            $this->cell(25, 6, $col[0], 1, '', 'C');
+            $this->cell(25, 6, $col[1], 1, '', 'C');
             $this->Ln(8);
         }
-        
 
-      
+     
+
         // $this->Ln(5);
     }
 
 
     function Footer()
-    {
+    { 
         // Posición: a 1,5 cm del final
         $this->SetY(-18);
         // Arial italic 8
@@ -251,7 +275,7 @@ class PDF extends FPDF
         $this->SetY(-10);
         $this->Cell(0, 5, 'Página ' . $this->PageNo() . '/{nb}', 0, 0, 'R');
         // opacidad
-        $this->SetAlpha(0.1);  
-         $this->Image("../../api/upload/".substr($this->arry3[0]['fe_log'],2), 46, 130, 120);
+        $this->SetAlpha(0.1);
+        $this->Image("../../api/upload/" . substr($this->arry3[0]['fe_log'], 2), 30, 110, 90);
     }
 }
--- compass/view/documento-A4.php
+++ compass/view/documento-A4.php
@@ -63,61 +63,64 @@ $fpdf->SetFont('Arial','',8);
 /////////////////item de productos///////////////
 
 foreach ($arry2 as $arr) {
-    $fpdf->cell(20,6,$arr['pro_cod'],1,'','L');
-    $fpdf->cell(90,6,substr($arr['pro_nom'],0,60),1,'','L');
-    $fpdf->cell(20,6,$arr['vd_can'] . " " . $arr['pst_nom'],1,'','C');
-    $fpdf->cell(30,6,"S/. " .  number_format($arr['vd_pre'],2,',',' '),1,'','R');
-    $fpdf->cell(30,6,"S/. " . number_format($arr['vd_pre'] * $arr['vd_can'],2,',',' '),1,'','R');
+    $fpdf->cell(20,6,$arr['pro_cod'],0,'','L');
+    $fpdf->MultiCell(90, 6,$arr['pro_nom'], 0, 'L');
+    $fpdf->SetXY(120, 68);
+    // $fpdf->MultiCell(90,6,$arr['pro_nom'],1,'L',true);
+    $fpdf->cell(15,6,$arr['vd_can'] ,0,'','C');
+    $fpdf->cell(15,6, $arr['pst_nom'],0,'','C');
+    $fpdf->cell(25,6,"S/. " .  number_format($arr['vd_pre'],2,',',' '),0,'','R');
+    $fpdf->cell(25,6,"S/. " . number_format($arr['vd_pre'] * $arr['vd_can'],2,',',' '),0,'','R');
     $fpdf->Ln();
 }
 
 /////////////////item de productos end///////////
 
+$fpdf->Ln(125);
 $num = new Num2letras();
-
-$fpdf->cell(130,6,'Son: '.$num->numero(round(floatval($arry[0]['ven_totdoc']), 3)),0,'','L');
+$fpdf->cell(140,6,'Son: '.$num->numero(round(floatval($arry[0]['ven_totdoc']), 3)),0,'','L');
 
 // $fpdf->Ln();
 // $fpdf->Image('qr.jpg',0,0,33);
 // $fpdf->Ln();
 // $fpdf->SetX(140);
-$fpdf->cell(30,6,'Afecto',1,'','L');
-$fpdf->cell(30,6,$op->montoAfecto($arry2),1,'','R');
+$fpdf->cell(25,6,'Afecto',1,'','L');
+$fpdf->cell(25,6,$op->montoAfecto($arry2),1,'','R');
 $fpdf->Ln();
 // $fpdf->SetY(50);
-$fpdf->SetX(140);
+$fpdf->SetX(150);
 // $fpdf->cell(130,36,'Inafecto',1,'','L');
-$fpdf->cell(30,6,'Inafecto',1,'','L');
-$fpdf->cell(30,6,$op->montoInafecto($arry2),1,'','R');
+$fpdf->cell(25,6,'Inafecto',1,'','L');
+$fpdf->cell(25,6,$op->montoInafecto($arry2),1,'','R');
 $fpdf->Ln();
 // $fpdf->SetX(140);
-$fpdf->Cell(130,30, $fpdf->Image('qr.jpg', $fpdf->GetX(0), $fpdf->GetY(0),25),0);
-$fpdf->cell(30,6,'ISC',1,'','L');
-$fpdf->cell(30,6, $op->montoIsc($arry2),1,'','R');
+$fpdf->Cell(140,30, $fpdf->Image('qr.jpg', $fpdf->GetX(0), $fpdf->GetY(0),25),0);
+$fpdf->cell(25,6,'ISC',1,'','L');
+$fpdf->cell(25,6, $op->montoIsc($arry2),1,'','R');
 $fpdf->Ln();
-$fpdf->SetX(140);
-$fpdf->cell(30,6,'IGV/IVA',1,'','L');
-$fpdf->cell(30,6,$op->montoIgv($arry2),1,'','R');
+$fpdf->SetX(150);
+$fpdf->cell(25,6,'IGV/IVA',1,'','L');
+$fpdf->cell(25,6,$op->montoIgv($arry2),1,'','R');
 $fpdf->Ln();
-$fpdf->SetX(140);
-$fpdf->cell(30,6,'Total Dsctos',1,'','L');
-$fpdf->cell(30,6,$op->montoDesc($arry2),1,'','R');
+$fpdf->SetX(150);
+$fpdf->cell(25,6,'Total Dsctos',1,'','L');
+$fpdf->cell(25,6,$op->montoDesc($arry2),1,'','R');
 $fpdf->Ln();
-$fpdf->SetX(140);
-$fpdf->cell(30,6,'Total Dcmto',1,'','L');
-$fpdf->cell(30,6,$op->montoDocum($arry2),1,'','R');
+$fpdf->SetX(150);
+$fpdf->cell(25,6,'Total Dcmto',1,'','L');
+$fpdf->cell(25,6,$op->montoDocum($arry2),1,'','R');
 $fpdf->Ln();
-$fpdf->SetX(140);
-$fpdf->cell(30,6,'Percepción',1,'','L');
-$fpdf->cell(30,6,number_format($arry2[0]['ven_per'],2,',',' '),1,'','R');
+$fpdf->SetX(150);
+$fpdf->cell(25,6,'Percepción',1,'','L');
+$fpdf->cell(25,6,number_format($arry2[0]['ven_per'],2,',',' '),1,'','R');
 $fpdf->Ln();
 // $fpdf->SetX(140);
-$fpdf->cell(130,6,'',0,'','L');
+$fpdf->cell(140,6,'',0,'','L');
 ;
-$fpdf->cell(30,6,'Total a Pagar',1,'','L');
-$fpdf->cell(30,6,$op->montoPagar($arry2),1,'','R');
+$fpdf->cell(25,6,'Total a Pagar',1,'','L');
+$fpdf->cell(25,6,$op->montoPagar($arry2),1,'','R');
 $fpdf->Ln();
-$fpdf->cell(130,6,'Vendedor: '.$fpdf->arry[0]['usuario']."  "."Cta Ahorro:  Cta Interbancaria: ",0,'','L');
+$fpdf->cell(140,6,'Vendedor: '.$fpdf->arry[0]['usuario']."  "."Cta Ahorro:  Cta Interbancaria: ",0,'','L');
 
 $fpdf->AliasNbPages();
 $fpdf->Output('I','nombre.pdf','UTF-8');
